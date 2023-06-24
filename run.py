7fdee0e5aef658a1fba08889cf69410d286925db
Mari Angeles Rojo Mira <maria.a.rojo.mira@gmail.com>
2023-06-24 13:48:46 +0000

    Add function to return a word

--- run.py
+++ run.py
@@ -2,4 +2,12 @@ import random
 from art import stages, logo
 from words import word_list
 
+"""
+Retuns a word for the game,
+it takes it from the imported list,
+converts all user input to uppercase.
+"""
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
 
